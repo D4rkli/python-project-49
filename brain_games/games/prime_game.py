fa4403153db7845b3226ddf4f4cd0b69f3e4e9bf
Darkl1 <anzhelikakosolapova@gmail.com>
2024-07-04 15:57:35 +0300

    Refactor code: Move common.py, update scripts and game modules, add shebang

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -4,7 +4,7 @@ import random
 def is_prime(number):
     if number <= 1:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
     return True
