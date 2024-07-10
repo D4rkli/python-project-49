4ae54e7b10f244ad1ab728f843fa64b663d568a3
Darkl1 <anzhelikakosolapova@gmail.com>
2024-07-10 10:58:21 +0300

    Fix errors

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from brain_games.common import play_game
-from brain_games.games.calc_game import generate_question_and_answer, DESCRIPTION
+from brain_games.games.calc_game import (
+    generate_question_and_answer,
+    DESCRIPTION
+)
 
 
 def main():
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 from brain_games.common import play_game
-from brain_games.games.even_game import generate_question_and_answer, DESCRIPTION
+from brain_games.games.even_game import (
+    generate_question_and_answer,
+    DESCRIPTION
+)
 
 
 def main():
